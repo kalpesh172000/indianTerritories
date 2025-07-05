b0910c84fa66c6f0b328a97bfc6911b99d3b11c1
kalpesh <kalpesh200071@gmail.com>
2025-07-05 07:43:50 +0000

    added logging functionality to project

---
+++ config/logger.js
@@ -0,0 +1,12 @@
+import logger from "signale"
+
+
+logger.config({
+    displayFilename: false,
+    displayTimestamp: true,
+    displayBadge: false,
+    displayDate: false,
+})
+
+
+export default logger;
